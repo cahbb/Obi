2360ee52b430223271c2afba96826e30f7aacbd7
Casper Bornebusch <cahbb9@gmail.com>
2016-12-06 19:40:21 +0100

    v0.0.0.1

---
+++ README.md
@@ -0,0 +1,8 @@
+# Obi #
+Obi / CSS3 Flexbox based light-weight CSS-framework, supported by the magic Flexbox offers.
+
+
+development in progress … 
+EPIC
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,27 @@
 'use strict'
 
 var gulp = require('gulp'),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify'),
   rename = require('gulp-rename'),
   sass = require('gulp-sass'),
   cleanCSS = require('gulp-clean-css'),
-  sourcemaps = require('gulp-sourcemaps'),
-  rimraf = require('gulp-rimraf')
-
-
-/*========================================
-  JS
-========================================*/
-
-gulp.task('js', function() {
-  return gulp.src('src/js/*.js')
-    .pipe(sourcemaps.init())
-    .pipe(concat('app.js'))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('src/js/build'))
-        .pipe(rename('app.min.js'))
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(uglify())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('src/js/build'));
-})
-
+  rimraf = require('gulp-rimraf'),
+  autoprefixer = require('gulp-autoprefixer')
 
 /*========================================
   SASS-CSS
 ========================================*/
 
 gulp.task('css', function() {
-  return gulp.src('src/scss/app.scss')
-    .pipe(sourcemaps.init())
+  return gulp.src('src/scss/obi.scss')
     .pipe(sass())
-    .pipe(sourcemaps.write())
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
     .pipe(gulp.dest('src/css'))
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(cleanCSS())
-        .pipe(rename('app.min.css'))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('src/css'))
+    .pipe(cleanCSS())
+    .pipe(rename('obi.min.css'))
+    .pipe(gulp.dest('src/css'))
 })
 
 
@@ -52,15 +31,14 @@ gulp.task('css', function() {
 
 gulp.task('watch', function() {
     gulp.watch('src/scss/**/*.scss', ['css'])
-    gulp.watch('src/js/*.js', ['js'])
 })
 
 gulp.task('clean', function() {
-  return gulp.src(['src/js/build/*', 'src/css/*'], { read: false })
+  return gulp.src(['src/css/*'], { read: false })
     .pipe(rimraf())
 })
 
-gulp.task('build', ['js', 'css'])
+gulp.task('build', ['css'])
 
 gulp.task('default', ['clean'], function() {
     gulp.start('build')
